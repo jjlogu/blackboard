e17b42e3c9ff6c4c4ad5a44e7962cfebe90db2d8
Logu J <1691611+jjlogu@users.noreply.github.com>
2019-03-12 12:26:52 -0500

    prep

---
+++ hackerrank/arrays/n_rotation.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+# problem statement: https://www.hackerrank.com/rest/contests/master/challenges/ctci-array-left-rotation/download_pdf?language=English
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the rotLeft function below.
+def rotLeft(a, d):
+    if len(a) == d:
+        return a
+    for _ in range(d):
+        b = a[1:]
+        b.append(a[0])
+        a = b
+    return a
+
+
+if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+    print(result)
+
+    #fptr.write(' '.join(map(str, result)))
+    #fptr.write('\n')
+
+    #fptr.close()
+
