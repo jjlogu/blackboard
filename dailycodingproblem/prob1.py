c9f82e47ce514412b9c5e4ce2bee5ec21a819188
Logu J <1691611+jjlogu@users.noreply.github.com>
2019-03-22 13:54:39 -0500

    improvement

--- dailycodingproblem/prob1.py
+++ dailycodingproblem/prob1.py
@@ -2,9 +2,12 @@
 Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
 """
 
 def add_up_to_k(numbs, k):
+    # first version
     numbs.sort()
     size = len(numbs)
     for i in range(size):
@@ -15,6 +18,15 @@ def add_up_to_k(numbs, k):
                 return True
     return False
 
+def add_upto_k(numbs, k):
+    # in one pass
+    want = {}
+    for i in numbs:
+        if want.get(i):
+            return True
+        want[k-i] = True
+    return False
+
 if __name__ == "__main__":
-    print(add_up_to_k([10,15,3,7], 19))
-    print(add_up_to_k([], 19))
+    print(add_upto_k([10,15,3,7,19,0], 19))
+    print(add_upto_k([], 19))
