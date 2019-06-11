72726b2414da96b03e703601435b5af57055c868
Logu J <1691611+jjlogu@users.noreply.github.com>
2019-06-11 12:47:52 -0500

    .

--- ipc/amqp/consumer.py
+++ ipc/amqp/consumer.py
@@ -1,9 +1,15 @@
 from __future__ import absolute_import, unicode_literals
 
-from kombu import Connection, entity
+from kombu import Connection, entity, Exchange
+from kombu.simple import SimpleQueue
 from kombu.five import Empty
 from amqp_buffer import AmqpBuffer
-# import threading
+import threading
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='(%(threadName)-10s) %(message)s',
+)
 
 exchange = entity.Exchange(name="target_event", type="direct")
 
@@ -11,6 +17,7 @@ exchange = entity.Exchange(name="target_event", type="direct")
 #: If hostname, userid, password and virtual_host is not specified
 #: the values below are the default, but listed here so it can
 #: be easily changed.
+"""
 with Connection('amqp://guest:guest@192.168.157.128:5672//') as conn:
     #: SimpleQueue mimics the interface of the Python Queue module.
     #: First argument can either be a queue name or a kombu.Queue object.
@@ -31,6 +38,8 @@ with Connection('amqp://guest:guest@192.168.157.128:5672//') as conn:
 
         for message in messages:
             print(message.payload)
+"""
+
 
 ####
 #: If you don't use the with statement then you must aways
@@ -38,9 +47,51 @@ with Connection('amqp://guest:guest@192.168.157.128:5672//') as conn:
 #   queue.close()
 #   connection.close()
 
-"""
+
 class DT(threading.Thread):
-    def __init__(self, connection, exchange, queue_name):
+    def __init__(self, connection, queue_name):
+        super(DT, self).__init__()
+        self.queue_name = queue_name
         self.connection = connection
-        self.buffer = AmqpBuffer(self.connection, entity.Queue(queue_name, exchange=exchange))
-"""
+        self.exchange = Exchange(name="event1", type="direct")
+        self._queue = entity.Queue(queue_name, exchange=self.exchange, routing_key=queue_name)
+        self.queue = SimpleQueue(self.connection, self._queue)
+
+    def run(self):
+        while True:
+            try:
+                self.main_loop()
+            except:
+                logging.exception("Unknown Exception: ")
+
+    def main_loop(self):
+        messages = []
+        print("inside", self.queue_name)
+        messages.append(self.queue.get(block=True))
+        print("after ")
+        messages[-1].ack()
+
+        # Check if there is anymore messages
+        try:
+            while True:
+                messages.append(self.queue.get(block=True, timeout=1))
+                messages[-1].ack()
+        except Empty:
+            pass
+
+        for message in messages:
+            print(message.payload)
+
+if __name__ == "__main__":
+    conn = Connection('amqp://guest:guest@192.168.157.128:5672//')
+    threads =[]
+    for i in range(2):
+        t = DT(conn, "que"+str(i))
+        print("i is ", i)
+        t.start()
+        print("i is after ", i)
+        threads.append(t)
+
+    for t in threads:
+        #t.join()
+        pass
