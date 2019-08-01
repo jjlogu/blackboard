cc1aba5a3afbf64ada59e6d9e9e99182f22b018d
Logu J <1691611+jjlogu@users.noreply.github.com>
2019-07-31 22:03:27 -0500

    reverse proxy

---
+++ python/simple_https_server/refelect.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# Reflects the requests from HTTP methods GET, POST, PUT, and DELETE
+# Written by Nathan Hamiel (2010)
+# SImple https reflect server
+#
+#
+# To generate server.pem
+#   openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
+#
+
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from optparse import OptionParser
+import ssl
+
+class RequestHandler(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        
+        request_path = self.path
+        
+        print("\n----- Request Start ----->\n")
+        print(request_path)
+        print(self.headers)
+        print("<----- Request End -----\n")
+        
+        self.send_response(200)
+        self.send_header("Set-Cookie", "foo=bar")
+        self.end_headers()
+        
+    def do_POST(self):
+        
+        request_path = self.path
+        
+        print("\n----- Request Start ----->\n")
+        print(request_path)
+        
+        request_headers = self.headers
+        content_length = request_headers.getheaders('content-length')
+        length = int(content_length[0]) if content_length else 0
+        
+        print(request_headers)
+        if length:
+            print(self.rfile.read(length))
+        else:
+            print("length:",length)
+            print(self.rfile.read(10000))
+        print("<----- Request End -----\n")
+        
+        self.send_response(200)
+        self.end_headers()
+    
+    do_PUT = do_POST
+    do_DELETE = do_GET
+        
+def main():
+    port = 8080
+    print('Listening on localhost:%s' % port)
+    server = HTTPServer(('', port), RequestHandler)
+    server.socket=ssl.wrap_socket(server.socket, certfile="./server.pem", server_side=True)
+    server.serve_forever()
+
+        
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.usage = ("Creates an http-server that will echo out any GET or POST parameters\n"
+                    "Run:\n\n"
+                    "   reflect")
+    (options, args) = parser.parse_args()
+    
+    main()
